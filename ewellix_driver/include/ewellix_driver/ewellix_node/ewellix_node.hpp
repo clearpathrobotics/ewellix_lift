2e6601271945709af192c60a499d1512168413c1
Ayush Singh <64409716+Ayush1285@users.noreply.github.com>
2025-04-09 16:11:28 +0530

    Added upper and lower limits as member variables

--- ewellix_driver/include/ewellix_driver/ewellix_node/ewellix_node.hpp
+++ ewellix_driver/include/ewellix_driver/ewellix_node/ewellix_node.hpp
@@ -90,6 +90,8 @@ private:
   float rated_effort_;
   float tolerance_;
   float frequency_;
+  int encoder_upper_limit_;
+  int encoder_lower_limit_;
   rclcpp::TimerBase::SharedPtr run_timer_;
 
   std::vector<int>encoder_positions_, encoder_commands_;
